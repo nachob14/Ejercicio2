47c789cf4b7ad45c5af39213367ef9bbd5fb2ade
nachob14 <98466036+nachob14@users.noreply.github.com>
2023-01-23 12:56:36 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+/* Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+*/
+
+const lista = ['Juan Ignacio', 23, false, new Date(1999, 11, 14), {titulo: 'La naranja mecanica', autor: 'Anthony Burgess', fecha: 1962, url: 'https://articulo.mercadolibre.com.ar/MLA-871294836-la-naranja-mecanica-anthony-burgess-minotauro-carontelibros-_JM#position=4&search_layout=stack&type=item&tracking_id=c84220c8-a363-45b0-b7ae-d33b2dd11502'}]
+
+console.log(lista);
